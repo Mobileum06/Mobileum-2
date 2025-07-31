58762e76d0fdd30f4c600376acfda837970e2db0
manoj <manojchauhan1300@gmail.com>
2025-07-31 16:22:42 +0530

    Initial commit - Terraform code for Azure RG

---
+++ mai.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "def13e29-f65a-496b-b10e-3cffca9a480f"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "my-first-rg"
+  location = "East US"
+}
